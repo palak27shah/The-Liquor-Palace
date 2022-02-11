200ebda47ae44b2c3a8d75070601417b43787410
Palak <palakchirag.shah@dcmail.ca>
2022-02-10 19:48:32 -0500

    Foundation css added in functions file to locate.
    To easily locate foundation css file we added it in functions file below enqueue scripts.

--- themes/drinks/functions.php
+++ themes/drinks/functions.php
@@ -126,7 +126,19 @@ add_action( 'widgets_init', 'drinks_widgets_init' );
  * Enqueue scripts and styles.
  */
 function drinks_scripts() {
-	wp_enqueue_style( 'drinks-style', get_stylesheet_uri(), array(), DRINKS_VERSION );
+	wp_enqueue_style( 
+		'drinks-style', 
+		get_stylesheet_uri(), 
+		array(), 
+		DRINKS_VERSION 
+	);
+
+	wp_enqueue_style( 
+		'foundation-style', 
+		get_template_directory_uri() . '/assets/css/vendor/foundation.min.css', 
+		array(), 
+		'6.7.4' 
+	);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
