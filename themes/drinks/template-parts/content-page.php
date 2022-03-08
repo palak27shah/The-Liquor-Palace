29c26b5d07c85a1f444392e2825156daee434e7d
Palak Shah <palakchirag.shah@dcmail.ca>
2022-03-08 11:17:17 -0500

    Modify title display condition
    Display page title in certain cases (cart, account page), and no title for the rest of the pages (Home, History etc.)

--- themes/drinks/template-parts/content-page.php
+++ themes/drinks/template-parts/content-page.php
@@ -11,7 +11,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
+		if ( is_page( 'shop' ) || is_page( 'cart' ) || is_page( 'checkout' ) || is_page( 'my-account' )) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		endif;
+		?>
 	</header><!-- .entry-header -->
 
 	<?php drinks_post_thumbnail(); ?>
--- themes/drinks/template-parts/content.php
+++ themes/drinks/template-parts/content.php
@@ -14,7 +14,9 @@
 		<?php
 		if ( ! is_singular( 'product') ) {
 			if ( is_singular() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				if ( is_page( 'shop' ) || is_page( 'cart' ) || is_page( 'checkout' ) || is_page( 'my-account' )) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				endif;
 			else :
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			endif;
