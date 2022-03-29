9a05c7dd1e85aa8701852acf6d0067f5c33ed058
Palak Shah <palakchirag.shah@dcmail.ca>
2022-03-29 15:10:28 -0400

    Conditional tag and Documenting
    Adding conditional tag to display main footer on certain pages only, add comments to make core more readable.

--- themes/drinks/footer.php
+++ themes/drinks/footer.php
@@ -47,34 +47,44 @@
                 ?>
             </div>
         </div>
-		<div class="custom-footer-row grid-container">
+        <?php   // Conditional tag to display site footer on certain pages only
+            if( is_singular() ) {
+                if( !is_page('contact') ) {
+        ?>
+            <div class="custom-footer-row grid-container">
             <div class="grid-x grid-margin-x grid-margin-y">
-                <!-- Footer column-1 - Made for quick links menu -->
+                <!-- Footer column-1 - Made for quick links(footer) menu -->
                 <?php if ( is_active_sidebar( 'footer-area-column-1' ) ) : ?>
                     <div class="footer-area-column-1 cell small-12 medium-3">
                         <?php dynamic_sidebar( 'footer-area-column-1' ); ?>
                     </div>
                 <?php endif; ?>
-                <!-- Footer column-2 -->
+                <!-- Footer column-2 - Made to show the location in the maps -->
                 <?php if ( is_active_sidebar( 'footer-area-column-2' ) ) : ?>
                     <div class="footer-area-column-2 cell small-12 medium-3">
                         <?php dynamic_sidebar( 'footer-area-column-2' ); ?>
                     </div>
                 <?php endif; ?>
-                <!-- Footer column-3 -->
+                <!-- Footer column-3 - Social media links navigation -->
                 <?php if ( is_active_sidebar( 'footer-area-column-3' ) ) : ?>
                     <div class="footer-area-column-3 cell small-12 medium-3">
                         <?php dynamic_sidebar( 'footer-area-column-3' ); ?>
                     </div>
                 <?php endif; ?>
-                <!-- Footer column-4 -->
+                <!-- Footer column-4 - Newsletter form created -->
                 <?php if ( is_active_sidebar( 'footer-area-column-4' ) ) : ?>
                     <div class="footer-area-column-4 cell small-12 medium-3">
                         <?php dynamic_sidebar( 'footer-area-column-4' ); ?>
                     </div>
                 <?php endif; ?>
-            </div>
-		</div>
+            </div> <!-- grid-x grid-margin-x grid-margin-y -->
+		</div> <!-- grid-container -->
+        <?php
+                }
+            }
+        ?>
+
+        <!-- Copyright and site information for the privacy of the site -->
 		<div class="site-info">
 			<?php
 				/* translators: 
